f90d74f98cd02213237baae176963c507d7f30d8
adil niaz <niaz.adil1@gmail.com>
2018-10-15 22:30:11 +0200

    files updated accordingly

--- src/Room.ts
+++ src/Room.ts
@@ -1,4 +1,4 @@
-interface IRoom {
+export interface IRoom {
 	id: string;
 	rooms: string[];
 	chests: string[];
@@ -8,6 +8,7 @@ export class Room implements IRoom {
 	id: string;
 	rooms: string[];
 	chests: string[];
+
 	constructor(id: string, rooms: string[], chests: string[]) {
 		this.id = id;
 		this.rooms = rooms;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-export function run() {
-	console.log(`Hello world`);
+import { URL } from 'url';
+
+import { Roamer } from './Roamer';
+
+const url = 'http://castles.poulpi.fr/castles/1/rooms/entry';
+
+// import * as Roamer from './Roamer';
+
+export async function run() {
+
+	const query = new URL(url).pathname;
+
+	const base = new URL(url).hostname;
+
+	console.log(new Date());
+	let roamer = new Roamer(base, query);
+	roamer.beginSearching();
 }
 
 run();
